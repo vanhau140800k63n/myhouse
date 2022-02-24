8b87aaf555e4e8168cea9eab2ac685e5aebc507c
vanhau <vanhau140800@gmail.com>
2022-02-24 17:31:39 +0700

    sidebar setup

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name = "csrf-token" content = "{{csrf_token ()}}">
+
+	<script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+	<div class="flex h-screen">
+		<div class="w-2/12 bg-[#343a40] px-[8px]">@include('partial.sidebar')</div>
+		<div class="w-10/12">@yield('content')</div>
+	</div>
+</body>
+</html>
---
+++ resources/views/partial/sidebar.blade.php
@@ -0,0 +1,8 @@
+<div class="flex flex-col">
+  <div class="text-white">MY HOUSE</div>
+  <div class="h-[40px] bg-[#ebecec] mb-[5px] px-[16px] py-[8px] rounded-[5px] text-[#343a40]">1</div>
+  <div class="h-[40px] bg-[#ebecec] mb-[5px] px-[16px] py-[8px] rounded-[5px] text-white">2</div>
+  <div class="h-[40px] bg-[#ebecec] mb-[5px] px-[16px] py-[8px] rounded-[5px]">3</div>
+  <div class="h-[40px] bg-[#ebecec] mb-[5px] px-[16px] py-[8px] rounded-[5px]">4</div>
+  <div class="h-[40px] bg-[#ebecec] mb-[5px] px-[16px] py-[8px] rounded-[5px]">5</div>
+</div>
